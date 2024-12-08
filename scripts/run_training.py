ef1f884c378a3857c1f97c56ff32df1245d422e2
Blina Smailaj <74669741+blinasmailaj@users.noreply.github.com>
2024-12-08 18:27:07 +0100

    Added Scripts

---
+++ scripts/run_training.py
@@ -0,0 +1,8 @@
+import sys
+import os
+import logging
+
+# Add project root to path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from src.config import ProjectConfig

fdb6e85f40c3301c84a766c606843372ff44e8c9
FlorianHalimi <florian.halimi01@gmail.com>
2024-12-21 11:40:28 +0100

    Removed some unnecessary folders because we moved the data loader to data folder
    Co-Authored-By: Blina Smailaj <74669741+blinasmailaj@users.noreply.github.com>

--- scripts/run_training.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-import logging
-
-# Add project root to path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from src.config import ProjectConfig
